f2a61cf6d6c37d6912c5b0028b5556e6f85c7e75
AlejandroRivas <rivasalejandro232323@gmail.com>
2024-09-15 06:45:45 -0600

    Change enviroment variables

--- back/src/config/AppDataSource.ts
+++ back/src/config/AppDataSource.ts
@@ -2,18 +2,21 @@ import { DataSource } from 'typeorm';
 import { User } from '../entities/User.entity';
 import { Credential } from '../entities/Credential.entity';
 import { Appointment } from '../entities/appointment.entity';
-import { DATABASE, PASSWORD, PORT_DATABASE, USERNAME } from './env';
+import { DATABASE, PASSWORD, PORT_DATABASE, INFOUSER } from './env';
 
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
     port: PORT_DATABASE,
-    username: USERNAME,
+    username: INFOUSER,
     password: PASSWORD,
     database: DATABASE,
     synchronize: true,
     logging: true,
+    dropSchema: true,
     entities: [User, Credential, Appointment],
     subscribers: [],
     migrations: [],
 });
+
+//export const makeRepository = (entity:any) => AppDataSource.getRepository(entity)
--- back/src/config/env.ts
+++ back/src/config/env.ts
@@ -1,11 +1,10 @@
 import "dotenv/config"
 
-console.log(process.env.PORT)
 
 export const PORT:number = Number(process.env.PORT);
 export const PASSWORD:string|undefined = process.env.PASSWORD;
 export const DATABASE:string|undefined = process.env.DATABASE;
-export const USERNAME:string|undefined = process.env.USERNAME;
+export const INFOUSER:string|undefined = process.env.INFOUSER;
 export const PORT_DATABASE:number = Number(process.env.PORT_DATABASE);
 
 
