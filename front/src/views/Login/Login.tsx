b1cffe6211b4b8957101075dbf8ee367f97afe1d
AlejandroRivas <rivasalejandro232323@gmail.com>
2024-10-15 10:02:04 -0600

    modifed for get and use infoUser after login

--- front/src/views/Login/Login.tsx
+++ front/src/views/Login/Login.tsx
@@ -2,20 +2,31 @@ import { ErrorMessage, Field, Form, Formik } from "formik";
 import style from './Login.module.css';
 import validateLogin from "../../helpers/validateLogin";
 import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { addInfoUser } from "../../redux/reducers";
 
 
-const URL = "http://localhost:3000/user/login"
+const URL = "http://localhost:3000/user/login";
 
 export default function Login(){
+    const user = useSelector((state) => state.userAppointments);
+    const dispatch = useDispatch();
 
     const handleSubmit = (values, {resetForm}) =>{
 
+
+        console.log("Before",user);
+        
         axios.post(URL, values)
-        .then((res) => console.log(res))
+        .then(({data}) => dispatch(addInfoUser(data)))
         .then( () => resetForm() )
         .catch((error) =>{
             console.log(error.message)
         })
+
+        
+        console.log("After",user);
+        
     }
 
     return(
@@ -35,12 +46,12 @@ export default function Login(){
                     ({isValid})=>(
                         <Form  className={style.form__form}>
                             <div className={style.form__field}>
-                                <label htmlFor="username">username</label>
+                                <label htmlFor="username" >username</label>
                                 <Field className={style.form__input}  type="text" name="username" placeholder="Ingrese su username"></Field>
                                 <ErrorMessage name="username" component="p"></ErrorMessage>
                             </div>
                             <div className={style.form__field}>
-                                <label htmlFor="password">password</label>
+                                <label htmlFor="password" >password</label>
                                 <Field  className={style.form__input} type="password" name="password" placeholder="Ingrese su password"></Field>
                                 <ErrorMessage className={style.form__errorsText} name="password" component="p"></ErrorMessage>
                             </div>
