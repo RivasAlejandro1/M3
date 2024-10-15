d5e434ff992792b5054821ca85416a1ff83bcdf8
AlejandroRivas <rivasalejandro232323@gmail.com>
2024-10-15 10:00:33 -0600

    modifed Nav with conditional rederization

--- front/src/components/Nav/Nav.tsx
+++ front/src/components/Nav/Nav.tsx
@@ -1,14 +1,24 @@
+import { useSelector } from "react-redux";
 import NavButton from "../NavButton/NavButton";
 import style from './Nav.module.css';
 
 
 export default function Nav (){
+    const currentUser = useSelector(state => state.user);
+
+
     return(
         <nav className={style.Nav}>
             
             <div className={style.logo}>CMB</div>
             <ul>
-                <NavButton path="register" name="Regiter"></NavButton>
+
+                {  
+                    currentUser ?              
+                    <NavButton path="myTurns" name="myTurns"></NavButton>
+                    : <></>
+                }  
+                <NavButton path="register" name="Register"></NavButton>
                 <NavButton path="about" name="About"></NavButton>
                 <NavButton path="home" name="Home"></NavButton>
                 <NavButton path="/" name="Log in"></NavButton>
