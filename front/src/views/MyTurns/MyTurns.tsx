de812bd11feb8b791bcf998e87a01e0cbef3eaea
AlejandroRivas <rivasalejandro232323@gmail.com>
2024-10-15 10:04:17 -0600

    modifed MyTurns with: global state and handleCancelAppointment

--- front/src/views/MyTurns/MyTurns.tsx
+++ front/src/views/MyTurns/MyTurns.tsx
@@ -3,29 +3,46 @@ import myAppointments from '../../helpers/myAppointments';
 import Turn from '../../components/Turn/Turn'
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { addInfoUserAppointment } from '../../redux/reducers';
 
 export default function MyTurns (){
+    
+    const URL = `http://localhost:3000/appointment/cancel/`
 
-    const [myAppointmentsState, setMyAppointmentsState ] = useState([]);
-    const handleAppointmentCancel = ()=>{
-        console.log("handleAppointmentCancel")
+    //? All about Redux
+    const currentUser = useSelector(state => state.user);
+    const currentAppointments = useSelector(state => state.userAppointments);
+    const dispatch = useDispatch();
 
+
+
+    const handleAppointmentCancel =  async (id:string)=>{
+        await axios.put(`${URL}${id}`)
+        .then(res => res.data)
+        .then(data => updateUserAppointments(currentUser.id))
+        .catch( error => console.log(error.message));
     } 
 
-    useEffect( ()=>{
-        axios.get("http://localhost:3000/appointment/")
+    const updateUserAppointments = async (id:string) => {
+        axios.get(`http://localhost:3000/appointment/user/${id}`)
         .then(({data}) => {
-            setMyAppointmentsState(data);
+            dispatch(addInfoUserAppointment(data));
         })
         .catch((error) =>{
-            console.log(error)
+            console.log(error);
         })
+
+    }
+    useEffect( ()=>{
+        if(currentUser?.id) updateUserAppointments(currentUser.id);
     }, [])
+
     return(
         <div className={style.MyTurns}>
             {
-                myAppointmentsState.length ? 
-                myAppointmentsState?.map((turn, index)=> {
+                currentAppointments?.length ? 
+                currentAppointments?.map((turn, index)=> {
                     const { status, date, time, details, id} = turn;
                     return( 
                         <Turn 
